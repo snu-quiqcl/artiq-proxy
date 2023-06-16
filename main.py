d9334869b417bb2fd79b841dd74d741390b93b58
BECATRUE <becatrueyjh@gmail.com>
2023-06-16 15:22:12 +0900

    Implement a function loading the setup file
    See also: #3

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
+from contextlib import asynccontextmanager
+import json
+
 from fastapi import FastAPI
+
+
+settings = {}
+
+
+def load_setup_file():
+    """Loads set-up information from the setup file.
+    
+    The file should have the following JSON structure:
+
+      {
+        "master_path": {master_path}
+      }
+    """
+    with open("setup.json", encoding="utf-8") as setup_file:
+        settings = json.load(setup_file)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    pass
+
 
 app = FastAPI()
 
