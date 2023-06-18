caa45f44d008102026965c5631ca52f7b7c9b6e9
BECATRUE <becatrueyjh@gmail.com>
2023-06-18 22:48:32 +0900

    Add the response model to get_experiment_info()
    See also: #5

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import json
 import os
+import pydantic
 from contextlib import asynccontextmanager
 from typing import Any, Dict, List
 
@@ -50,8 +51,13 @@ async def list_directory(directory: str = "") -> List[str]:
     return remote.list_directory(os.path.join(configs["master_path"], directory))
 
 
-@app.get("/experiment/info/")
-async def get_experiment_info(file: str) -> Dict[str, Dict[str, Any]]:
+class ExperimentInfo(pydantic.BaseModel):
+    name: str
+    arginfo: Dict[str, Any]
+
+
+@app.get("/experiment/info/", response_model=Dict[str, ExperimentInfo])
+async def get_experiment_info(file: str) -> Any:
     """Get information of the given experiment file and returns it.
     
     Args:
@@ -59,9 +65,12 @@ async def get_experiment_info(file: str) -> Dict[str, Dict[str, Any]]:
 
     Returns:
         A dictionary containing only one element of which key is the class name.
-        The value is a dictionary with four keys (show only two important things):
-          name: The experiment name.
+        The value is a dictionary with two keys:
+          name: The experiment name which is set as the docstring in the experiment file.
           arginfo: The dictionary containing arguments of the experiment.
+            Each key is an argument name and its value contains the argument type,
+            the default value, and the additional information for the argument.
+
     """
     remote = get_client("master_experiment_db")
     return remote.examine(file)
