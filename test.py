690a59e6d95594cc77715768623b556b6f70325d
BECATRUE <becatrueyjh@gmail.com>
2023-06-19 13:02:42 +0900

    Add a test file
    See also: #7

---
+++ test.py
@@ -0,0 +1,15 @@
+"""Unit tests for main module."""
+
+import unittest
+from unittest import mock
+from fastapi.testclient import TestClient
+
+import main
+
+
+class Test(unittest.TestCase):
+    """Unit tests for routing and each operation."""
+
+
+if __name__ == "__main__":
+    unittest.main()
