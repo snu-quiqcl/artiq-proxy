14413a67a96831b8a1350c1a1adf35e37c85aea0
giwon2004 <giwons04@naver.com>
2023-08-07 06:26:25 +0900

    add docstring

--- main.py
+++ main.py
@@ -110,14 +110,22 @@ async def get_experiment_code(file: str) -> str:
 
 @app.post("/experiment/delete/")
 async def delete_experiment(rid):
-    """Kills the run with the specified RID."""
+    """Kills the run with the specified RID.
+
+    Args:
+        rid: rid value of the target experiment.
+    """
     remote = get_client("master_schedule")
     remote.delete(rid)
 
 
 @app.post("/experiment/request_termination/")
-async def delete_experiment(rid):
-    """Requests graceful termination of the run with the specified RID."""
+async def request_termination_of_experiment(rid):
+    """Requests graceful termination of the run with the specified RID.
+
+    Args:
+        rid: rid value of the target experiment.
+    """
     remote = get_client("master_schedule")
     remote.request_termination(rid)
 
