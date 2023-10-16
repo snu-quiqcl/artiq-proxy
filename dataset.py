14c9c73d31e7b3ab8443452ac7399fe08076f31d
Jiyong Kang <kangz12345@gmail.com>
2023-10-16 11:48:02 +0900

    Implement `since()`

--- dataset.py
+++ dataset.py
@@ -1,5 +1,7 @@
 """Module for realtime dataset management."""
 
+import bisect
+import itertools
 from collections import deque
 from typing import Any, Dict, Optional, Tuple, TypeVar, Generic
 
@@ -32,6 +34,23 @@ class SortedQueue(Generic[K, V]):
         The queue must not be empty.
         """
         return self._keys.popleft(), self._values.popleft()
+    
+    def since(self, key: K) -> Tuple[K, Tuple[V]]:
+        """Returns the most recent key and the values since the given key.
+        
+        Args:
+            key: The search point key.
+        
+        Returns:
+            (latest_key, values): latest_key is the biggest key, including the
+              given key. All the values whose keys are bigger than the given key
+              are returned in a tuple. If the given key is the biggest, the
+              return value will be (key, ()).
+        """
+        i = bisect.bisect(self._keys, key)
+        latest_key = self._keys[-1] if i < len(self._keys) else key
+        values = tuple(itertools.islice(self._values, i, None))
+        return latest_key, values
 
 
 class DatasetTracker:
