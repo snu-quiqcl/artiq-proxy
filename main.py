283521599d28f9ceca1a084d43f75d337cb441ec
Jiyong Kang <kangz12345@gmail.com>
2023-10-17 10:44:47 +0900

    Apply the new config structure

--- main.py
+++ main.py
@@ -113,7 +113,7 @@ def init_dataset_tracker() -> Subscriber:
     This should be called after loading config.
     """
     global dataset_tracker
-    maxlen = configs.get("dataset_tracker_maxlen", 1 << 16)
+    maxlen = configs["dataset_tracker"].get("maxlen", 1 << 16)
     dataset_tracker = dset.DatasetTracker(maxlen)
     return Subscriber("datasets", lambda x: x, functools.partial(dset.notify_callback, dataset_tracker))
 
