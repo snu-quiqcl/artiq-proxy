daba31fc6787b1bc1a52b696c5e59af186549c67
BECATRUE <becatrueyjh@gmail.com>
2023-11-01 08:50:32 +0900

    Define schedule tracker
    See also: #102

--- schedule.py
+++ schedule.py
@@ -1,1 +1,9 @@
 """Module for realtime schedule management."""
+
+from typing import Any, Optional
+
+class ScheduleTracker:
+    """Holds schedule in real time."""
+
+    def __init__(self):
+        self._target: Optional[dict[int, Any]] = None
