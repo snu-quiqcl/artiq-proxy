ee33569748c30eb3bd4d8c124c41ef4fe1b4f1c3
BECATRUE <becatrueyjh@gmail.com>
2023-11-01 11:12:02 +0900

    Implement `notify_modified()`
    See also: #102

--- schedule.py
+++ schedule.py
@@ -15,8 +15,16 @@ class ScheduleTracker(Tracker[dict[int, Any]]):
     def __init__(self):
         """Extended."""
         super().__init__()
-        self.modifed: Optional[asyncio.Event] = None
+        self.modifed = asyncio.Event()
 
     def get(self) -> dict[int, Any]:
         """Returns the current schedule."""
         return self._target
+    
+    def notify_modified(self):
+        """Sets and clears the modified event.
+        
+        All the coroutines that are waiting for the modified event will be awakened.
+        """
+        self.modifed.set()
+        self.modifed.clear()
