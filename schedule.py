95031007746b4e4e105e3c9b13fc30a2ddbf87a4
BECATRUE <becatrueyjh@gmail.com>
2023-11-01 11:42:07 +0900

    Add `latest` timestamp in tracker
    See also: #102

--- schedule.py
+++ schedule.py
@@ -6,27 +6,33 @@ from typing import Any
 
 from tracker import Tracker
 
-class ScheduleTracker(Tracker[dict[int, Any]]):
+Schedule = dict[int, Any]
+
+
+class ScheduleTracker(Tracker[Schedule]):
     """Holds schedule in real time.
     
     Attributes:
         modified: The event set when any new modification to the schedule is added.
+        latest: The timestamp of the last modification.
     """
 
     def __init__(self):
         """Extended."""
         super().__init__()
         self.modifed = asyncio.Event()
+        self.latest = -1
 
-    def get(self) -> tuple[float, dict[int, Any]]:
+    def get(self) -> tuple[float, Schedule]:
         """Returns the current timestamp and schedule."""
-        return time.time(), self._target
+        return self.latest, self._target
     
     def notify_modified(self):
         """Sets and clears the modified event.
         
         All the coroutines that are waiting for the modified event will be awakened.
         """
+        self.latest = time.time()
         self.modifed.set()
         self.modifed.clear()
 
