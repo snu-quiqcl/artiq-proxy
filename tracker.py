659037854e4d595e6f9187dc626e16c1390c2f15
BECATRUE <becatrueyjh@gmail.com>
2023-11-01 09:21:00 +0900

    Implement `target_builder()`
    See also: #102

--- tracker.py
+++ tracker.py
@@ -9,3 +9,16 @@ class Tracker(Generic[T]):
 
     def __init__(self):
         self._target: Optional[T] = None
+
+    def target_builder(self, struct: T) -> T:
+        """Initializes the target with the given struct.
+        
+        See sipyco.sync_struct.Subscriber for details.
+
+        This will make self._target the synchronized structure of the notifier.
+
+        Args:
+            struct: The initial structure for the target.
+        """
+        self._target = struct
+        return self._target
