b2750c068ff864ad01fadccb001cef78779c01ea
BECATRUE <becatrueyjh@gmail.com>
2023-11-01 11:08:43 +0900

    Add modified event into schedule tracker
    See also: #102

--- schedule.py
+++ schedule.py
@@ -1,11 +1,21 @@
 """Module for realtime schedule management."""
 
-from typing import Any
+import asyncio
+from typing import Any, Optional
 
 from tracker import Tracker
 
 class ScheduleTracker(Tracker[dict[int, Any]]):
-    """Holds schedule in real time."""
+    """Holds schedule in real time.
+    
+    Attributes:
+        modified: The event set when any new modification to the schedule is added.
+    """
+
+    def __init__(self):
+        """Extended."""
+        super().__init__()
+        self.modifed: Optional[asyncio.Event] = None
 
     def get(self) -> dict[int, Any]:
         """Returns the current schedule."""
