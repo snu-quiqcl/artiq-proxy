97d954e148b7a306bd8dadc08186cf7c50033a54
BECATRUE <becatrueyjh@gmail.com>
2023-11-01 09:19:33 +0900

    Define tracker class
    See also: #102

--- tracker.py
+++ tracker.py
@@ -1,5 +1,11 @@
 """Module for base tracker."""
 
-from typing import TypeVar
+from typing import Generic, Optional, TypeVar
 
 T = TypeVar("T")
+
+class Tracker(Generic[T]):
+    """Base tracker class."""
+
+    def __init__(self):
+        self._target: Optional[T] = None
