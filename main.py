ddd422e2c47fc9a1782fd7a7fbc95cf0fdaa29db
BECATRUE <becatrueyjh@gmail.com>
2024-04-06 15:39:08 +0900

    Remove codes handling results

--- main.py
+++ main.py
@@ -257,90 +257,14 @@ async def request_termination_of_experiment(rid: int):
     remote.request_termination(rid)
 
 
-def add_tracking_line(stmt_list: list[ast.stmt]) -> list[ast.stmt]:
-    """Returns a new statements list interleaved with rtio logs for tracking each line.
-
-    Adds logs containing the corresponding line number before each line, recursively.
-    The given statements list is not modified.
-
-    Args:
-        stmt_list: The list of ast statements.
-
-    Returns:
-        The created list of ast statements.
-    """
-    modified_stmt_list = []
-    for stmt in stmt_list:
-        tracking_line_code = f"rtio_log('line', {stmt.lineno})"
-        tracking_line_stmt = ast.parse(tracking_line_code).body
-        modified_stmt_list.extend(tracking_line_stmt)
-        for attr in ("body", "handlers", "orelse", "finalbody"):
-            if hasattr(stmt, attr):
-                setattr(stmt, attr, add_tracking_line(getattr(stmt, attr)))
-        modified_stmt_list.append(stmt)
-    return modified_stmt_list
-
-
-def modify_experiment_code(code: str, experiment_cls_name: str) -> str:
-    """Modifies the given code to leave rtio logs and make the vcd file.
-
-    It performs the following modifications:
-        1. Add the rtio logs to track each line.
-        2. Make the vcd file into the corresponding path.
-
-    It assumes that the code contains an experiment class and run() method.
-
-    Args:
-        code: The experiment code.
-        experiment_cls_name: The class name of the experiment.
-
-    Returns:
-        The modified experiment code.
-    """
-    mod = ast.parse(code)
-    # import modules
-    import_stmt = ast.parse("import subprocess").body
-    mod.body = import_stmt + mod.body
-    experiment_cls_stmt = next(
-        stmt for stmt in mod.body
-        if isinstance(stmt, ast.ClassDef) and stmt.name == experiment_cls_name
-    )
-    run_func_stmt = next(
-        stmt for stmt in experiment_cls_stmt.body
-        if isinstance(stmt, ast.FunctionDef) and stmt.name == "run"
-    )
-    # make logs to track each line
-    run_func_stmt.body = add_tracking_line(run_func_stmt.body)
-    # call write_vcd()
-    write_vcd_call_stmt = ast.parse("self.write_vcd()").body
-    run_func_stmt.body.append(write_vcd_call_stmt)
-    # define write_vcd()
-    device_db_path = posixpath.join(configs["master_path"], "device_db.py")
-    vcd_path = posixpath.join(configs["master_path"], configs["result_path"], "_{rid}/rtio.vcd")
-    write_vcd_func_code = f"""
-def write_vcd(self):
-    result = subprocess.run("curl http://127.0.0.1:8000/experiment/running/",
-                            capture_output=True, shell=True, text=True)
-    rid = int(result.stdout)
-    device_db_path = "{device_db_path}"
-    vcd_path = f"{vcd_path}"
-    subprocess.run(f"artiq_coreanalyzer --device-db {{device_db_path}} -w {{vcd_path}}",
-                   capture_output=True, shell=True)
-    """
-    write_vcd_func_stmt = ast.parse(write_vcd_func_code).body
-    experiment_cls_stmt.body.append(write_vcd_func_stmt)
-    return ast.unparse(mod)
-
-
 @app.get("/experiment/submit/")
-async def submit_experiment(  # pylint: disable=too-many-arguments, too-many-locals
+async def submit_experiment(  # pylint: disable=too-many-arguments
     file: str,
     cls: Optional[str] = None,
     args: str = "{}",
     pipeline: str = "main",
     priority: int = 0,
     timed: Optional[str] = None,
-    visualize: bool = False,
 ) -> int:
     """Submits the given experiment file.
     
@@ -353,33 +277,11 @@ async def submit_experiment(  # pylint: disable=too-many-arguments, too-many-loc
         priority: Higher value means sooner scheduling.
         timed: The due date for the experiment in ISO format.
           None for no due date.
-        visualize: If True, the experiment file is modified for visualization.
-          The original file and vcd file are saved in the visualize path set in config.json.
-          The cls argument should be given.
     
     Returns:
         The run identifier, an integer which is incremented at each experiment submission.
     """
-    submission_time = datetime.now().isoformat()
-    result_dir_path = posixpath.join(configs["master_path"], configs["result_path"])
-    if visualize:
-        src_experiment_path = posixpath.join(
-            configs["master_path"], configs["repository_path"], file
-        )
-        modified_experiment_path = posixpath.join(
-            result_dir_path,
-            f"experiment_{submission_time}.py"
-        )
-        # modify the experiment code
-        with open(src_experiment_path, encoding="utf-8") as experiment_file:
-            code = experiment_file.read()
-        modified_code = modify_experiment_code(code, cls)
-        # save the modified experiment code
-        with open(modified_experiment_path, "w", encoding="utf-8") as modified_experiment_file:
-            modified_experiment_file.write(modified_code)
-        submission_file_path = modified_experiment_path
-    else:
-        submission_file_path = posixpath.join(configs["repository_path"], file)
+    submission_file_path = posixpath.join(configs["repository_path"], file)
     args_dict = json.loads(args)
     expid = {
         "log_level": logging.WARNING,
@@ -390,21 +292,6 @@ async def submit_experiment(  # pylint: disable=too-many-arguments, too-many-loc
     due_date = None if timed is None else time.mktime(datetime.fromisoformat(timed).timetuple())
     remote = get_client("master_schedule")
     rid = remote.submit(pipeline, expid, priority, due_date, False)
-    # make the RID directory
-    rid_dir_path = posixpath.join(result_dir_path, f"_{rid}/")
-    os.makedirs(rid_dir_path)
-    # save the metadata
-    metadata = {
-        "submission_time": submission_time,
-        "visualize": visualize
-    }
-    metadata_path = posixpath.join(rid_dir_path, "metadata.json")
-    with open(metadata_path, "w", encoding="utf-8") as metadata_file:
-        json.dump(metadata, metadata_file)
-    if visualize:
-        # copy the original experiment file
-        dst_experiment_path = posixpath.join(rid_dir_path, "experiment.py")
-        shutil.copyfile(src_experiment_path, dst_experiment_path)
     return rid
 
 
@@ -423,136 +310,6 @@ async def get_status(rid: int) -> Optional[dict]:
     remote = get_client("master_schedule")
     status = remote.get_status()
     return status.get(rid, None)
-
-
-@app.get("/experiment/running/")
-async def get_running_experiment() -> Optional[int]:
-    """Gets the running experiment RID.
-
-    It assumes that there is at most one running experiment.
-    
-    Returns:
-        The run identifier of the running experiment.
-        If no running experiment, it returns None.
-    """
-    remote = get_client("master_schedule")
-    status = remote.get_status()
-    for rid, info in status.items():
-        if info["status"] == "running":
-            return rid
-    return None
-
-
-# pylint: disable=too-many-locals
-def organize_result_directory(result_dir_path: str, rid: str) -> bool:
-    """Organizes the result directory.
-    
-    It performs the following:
-        1. Read the metadata file from the RID directory.
-        2. Move the modified experiment to the RID directory.
-        3. Find the h5 result file and copy it to the RID directory.
-        4. Dump the metadata on the copied result file.
-        5. Remove the metadata file.
-
-    Args:
-        result_dir_path: The full path of the result directory.
-        rid: The run identifier value of the experiment in string.
-
-    Returns:
-        True if the h5 result file exists.
-        Otherwise, stop organizing the result directory and return False. 
-    """
-    rid_dir_path = posixpath.join(result_dir_path, f"_{rid}/")
-    # read the metadata
-    metadata_path = posixpath.join(rid_dir_path, "metadata.json")
-    try:
-        with open(metadata_path, encoding="utf-8") as metadata_file:
-            metadata = json.load(metadata_file)
-    except OSError:  # no chance of happening
-        logger.exception("The RID %s directory has no metadata file.", rid)
-        return False
-    submission_time_str, visualize = metadata["submission_time"], metadata["visualize"]
-    submission_time = datetime.fromisoformat(submission_time_str)
-    date = submission_time.date().isoformat()
-    hour = str(submission_time.hour).zfill(2)
-    # find the result file
-    datetime_result_dir_path = posixpath.join(result_dir_path, f"{date}/{hour}/")
-    padded_rid = rid.zfill(9)
-    for item in os.listdir(datetime_result_dir_path):
-        if item.startswith(padded_rid):
-            break
-    else:  # the experiment has not yet been run
-        return False
-    # copy the result file to the RID directory
-    src_result_path = posixpath.join(datetime_result_dir_path, item)
-    dst_result_path = posixpath.join(rid_dir_path, "result.h5")
-    shutil.copyfile(src_result_path, dst_result_path)
-    # modify the result file
-    with h5py.File(dst_result_path, "a") as result_file:
-        # start time
-        start_time = result_file["start_time"][()]
-        del result_file["start_time"]
-        start_time_str = datetime.fromtimestamp(start_time).isoformat()
-        start_time_dataset = result_file.create_dataset(
-            "start_time", (1,), dtype=h5py.string_dtype(encoding="utf-8")
-        )
-        start_time_dataset[0] = start_time_str
-        # run time
-        run_time = result_file["run_time"][()]
-        del result_file["run_time"]
-        run_time_str = datetime.fromtimestamp(run_time).isoformat()
-        run_time_dataset = result_file.create_dataset(
-            "run_time", (1,), dtype=h5py.string_dtype(encoding="utf-8")
-        )
-        run_time_dataset[0] = run_time_str
-        # submission time
-        submission_time_dataset = result_file.create_dataset(
-            "submission_time", (1,), dtype=h5py.string_dtype(encoding="utf-8")
-        )
-        submission_time_dataset[0] = submission_time_str
-        # visualize option
-        visualize_dataset = result_file.create_dataset("visualize", (1,), dtype="bool")
-        visualize_dataset[0] = visualize
-    # move the modified experiment file to the RID directory
-    if visualize:
-        src_experiment_path = posixpath.join(
-            result_dir_path, f"experiment_{submission_time_str}.py"
-        )
-        dst_experiment_path = posixpath.join(rid_dir_path, "modified_experiment.py")
-        shutil.move(src_experiment_path, dst_experiment_path)
-    # remove the metadata file
-    os.remove(metadata_path)
-    return True
-
-
-@app.get("/result/")
-async def list_result_directory() -> list[int]:
-    """Post-processes the executed experiments and returns the RID list.
-
-    It performs the following:
-        1. Organize the result directory.
-        2. Return the RID list.
-
-    Returns:
-        A list with RIDs of the executed experiments, sorted in an ascending order.
-    """
-    result_dir_path = posixpath.join(configs["master_path"], configs["result_path"])
-    # navigate to each RID directory not organized yet
-    for item in os.listdir(result_dir_path):
-        item_path = posixpath.join(result_dir_path, item)
-        if posixpath.isdir(item_path) and item.startswith("_") and item[1:].isdigit():
-            rid = item[1:]
-            if organize_result_directory(result_dir_path, rid):
-                rid_dir_path = posixpath.join(result_dir_path, f"{rid}/")
-                os.rename(item_path, rid_dir_path)
-    # find all organized RID directories
-    rid_list = []
-    for item in os.listdir(result_dir_path):
-        item_path = posixpath.join(result_dir_path, item)
-        if posixpath.isdir(item_path) and item.isdigit():
-            rid_list.append(int(item))
-    rid_list.sort()
-    return rid_list
 
 
 @app.get("/rid/list/")
@@ -700,38 +457,6 @@ async def get_dataset_modification(websocket: WebSocket):
         logger.info("The connection for sending the dataset modification is closed.")
     except websockets.exceptions.WebSocketException:
         logger.exception("Failed to send the dataset modification.")
-
-
-class ResultFileType(str, Enum):
-    """Enum class for describing the result file type.
-    
-    H5: The H5 format result file, result.h5.
-    CODE: The original experiment file, experiment.py.
-    VCD: The VCD format file, rtio.vcd.
-    """
-    H5 = "h5"
-    CODE = "code"
-    VCD = "vcd"
-
-
-@app.get("/result/{rid}/{result_file_type}/")
-async def get_result(rid: str, result_file_type: ResultFileType) -> FileResponse:
-    """Gets the result file of the given RID for the given result file type and returns it.
-    
-    Args:
-        rid: The run identifier value in string.
-        result_file_type: The type of the requested result file in ResultFileType.
-    """
-    if result_file_type is ResultFileType.H5:
-        result_path = "result.h5"
-    elif result_file_type is ResultFileType.CODE:
-        result_path = "experiment.py"
-    else:
-        result_path = "rtio.vcd"
-    full_result_path = posixpath.join(
-        configs["master_path"], configs["result_path"], rid, result_path
-    )
-    return FileResponse(full_result_path)
 
 
 @app.websocket("/ttl/status/modification/")
