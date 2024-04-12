3ff12175bbcb8cf2d64a3d8744e22a636ff7d7a1
BECATRUE <becatrueyjh@gmail.com>
2024-04-12 11:45:14 +0900

    Support command line arguments in `server.sh`

--- server.sh
+++ server.sh
@@ -1,1 +1,23 @@
-python -m uvicorn main:app --host 0.0.0.0 --port 8000
+#!/bin/bash
+
+config_path=""
+
+helpFunction()
+{
+    echo "Usage $0 -c \"config_path\""
+    exit 1
+}
+
+while getopts "c:" opt
+do
+    case "$opt" in
+        c) config_path=$OPTARG ;;
+        *) echo "Invalid option" ; helpFunction ;;
+    esac
+done
+
+if [ -z "$config_path" ]; then
+    python -m uvicorn main:app --host 0.0.0.0 --port 8000
+else
+    CONFIG_PATH=$config_path python -m uvicorn main:app --host 0.0.0.0 --port 8000
+fi
